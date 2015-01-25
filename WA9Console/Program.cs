66544dbfd853ee7f1a49f456af68e4825a09feb4
aribilgiozgur <aribilgi.ozgur@yandex.com>
2015-01-25 12:01:59 +0200

    Initial Commit

---
+++ WA9Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WA9Console.WA9Service;
+
+namespace WA9Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WA9ServiceClient client = new WA9ServiceClient();
+            List<Item> items = client.getAllItems().ToList();
+            foreach (Item i in items)
+            {
+                Console.WriteLine(i.ItemName +"-"+i.ItemCount.ToString());
+            }
+
+            Item it = new Item();
+            it.ItemName = "Konsoldan...";
+            it.ItemCount = 99;
+            Console.WriteLine(client.insertItem(it));
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
